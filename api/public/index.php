1f70e98a7615cf11bfc5e9a11a32097ab474ca88
Vitor <vitorcorreia.git@gmail.com>
2025-09-24 20:17:06 -0300

    feat(public): adiciona arquivo index responsável pelo ponto de entrada da aplicação

--- api/public/index.php
+++ api/public/index.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Core\Core;
+use App\Http\Router;
 use Dotenv\Dotenv;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/src/routes/main.php';
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
- 
+
+Core::dispatch(Router::routes());
+
