4abfee7946a55b53d6764b7ee82790606ac4696b
Vitor <vitorcorreia.git@gmail.com>
2025-09-24 20:19:20 -0300

    feat(router): cria arquivo main para definição das rotas da aplicação

---
+++ api/src/routes/main.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Router;
+
+// Router::post("/auth/login", "AuthController:login");
+
+//Users 
+
+Router::get("/users", "UserController:fetch");
+Router::post("/users", "UserController:store");
+Router::put("/users", "UserController:update");
+Router::delete("/users/{id}", "UserController:delete");
+
+//Accounts
+
+// Router::get("/accounts/fetch", "AccountsController:fetch");
+// Router::post("/accounts/create", "AccountsController:store");
+// Router::put("/accounts/update", "AccountsController:update");
+// Router::delete("/accounts/delete/{id}", "AccountsController:delete");
+
+
+
+
+
+?>
