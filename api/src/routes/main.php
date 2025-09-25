1e164bf7cbffbd62f708a027f1dab80a985e04f7
Vitor <vitorcorreia.git@gmail.com>
2025-09-25 10:49:00 -0300

    refactor: modifica rota existente

--- api/src/routes/main.php
+++ api/src/routes/main.php
@@ -6,7 +6,7 @@ Router::post("/auth/login", "AuthController:login");
 
 //Users 
 
-Router::get("/users", "UserController:fetch");
+Router::get("/users", "UserController:show");
 Router::post("/users", "UserController:store");
 Router::put("/users", "UserController:update");
 Router::delete("/users/{id}", "UserController:delete");
