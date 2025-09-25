446c937b89a80ac1daabf626b1af92d89c4f06e7
Vitor <vitorcorreia.git@gmail.com>
2025-09-25 09:23:04 -0300

    feat: adiciona método body para capturar dados do usuário

--- api/src/Http/Request.php
+++ api/src/Http/Request.php
@@ -12,4 +12,16 @@ class Request
         return $_SERVER['REQUEST_METHOD'];
     }
 
+    public static function body()
+    {
+        $json = json_decode(file_get_contents("php://input"), true) ?? [];
+
+        $data = match (self::methodHttp()) {
+            'GET' => $_GET,
+            'POST', 'PUT', 'DELETE' => $json,
+        };
+
+        return $data;
+    }
+
 }
